d8e16ca1ee100544a4bad738a8a2e2eacb6f3ac6
Aristo Michael <aristom@PDYW1002.ihorsetech.com>
2014-11-12 20:51:58 +0530

    Relative Layout Button updates

---
+++ gen/relativelayoutbutton/android/izvornikod/com/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package relativelayoutbutton.android.izvornikod.com;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
